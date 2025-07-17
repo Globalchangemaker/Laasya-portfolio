079b4a92d022ece30feeb70914b97fcfd92a9efd
Globalchangemaker <laasyagsssietw@gmail.com>
2025-07-17 12:41:13 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Scroll animation
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  const trigger = window.innerHeight * 0.8;
+
+  sections.forEach((section) => {
+    const top = section.getBoundingClientRect().top;
+    if (top < trigger) {
+      section.classList.add("visible");
+    }
+  });
+});
